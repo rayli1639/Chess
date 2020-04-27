e408817776dba8af564948840270689321a2ff53
rayli1639 <ray.z.li@gmail.com>
2020-04-27 15:28:10 -0400

    Created Game Object

---
+++ gameChess.py
@@ -0,0 +1,66 @@
+'''
+Created on Apr 27, 2020
+
+@author: RayL
+'''
+import pygame
+from board import Board
+
+class GameChess():
+    
+    def __init__ (self,window):
+        self.window = window
+        self.running = True
+        self.clock = pygame.time.Clock()
+        self.fps = 30
+        self.board = Board(self.window)
+        self.board.show()
+        self.board.drawBoard()
+        self.board.drawPieces()
+        self.possibleSpaces = []
+        self.isSelected = False
+        self.pieceSelected = 0
+        self.turn = 1
+        self.turnCol = 'white'
+        self.oppoCol = 'black'
+    
+    def play(self):
+        self.clock.tick(self.fps) #Update the clock by the fps every frame
+        for event in pygame.event.get(): #Loop to check for user exit
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                coords = [x // self.board.spaceSize for x in pygame.mouse.get_pos()]
+                coords.reverse()
+                clickedSpace = self.board.board[coords[0]][coords[1]]
+                if not self.isSelected:
+                    if clickedSpace != 0 and clickedSpace.color == self.turnCol:
+                        self.pieceSelected = clickedSpace
+                        if self.pieceSelected != 0:
+                            self.possibleSpaces = self.pieceSelected.getSpaces(self.board,self.window)
+                        self.isSelected = True
+                elif self.isSelected:
+                    if (clickedSpace == 0 or clickedSpace == self.pieceSelected or 
+                        clickedSpace.color == self.oppoCol):
+                        if coords in self.possibleSpaces:
+                            self.board.move(self.pieceSelected,coords)
+                            self.turn += 1
+                        self.pieceSelected = 0
+                        self.possibleSpaces = []
+                        self.isSelected = False
+                        self.board.drawBoard()
+                        self.board.drawPieces()
+                    else:
+                        self.board.drawBoard()
+                        self.board.drawPieces()
+                        if clickedSpace.color == self.turnCol:
+                            self.pieceSelected = self.board.board[coords[0]][coords[1]]
+                            self.possibleSpaces = self.pieceSelected.getSpaces(self.board,self.window)
+                            self.isSelected = True
+        if self.turn % 2 == 1:
+            self.turnCol = 'white'
+            self.oppoCol = 'black'
+        else:
+            self.turnCol = 'black'
+            self.oppoCol = 'white'
+        pygame.display.update()
--- main.py
+++ main.py
@@ -3,71 +3,14 @@ Created on Apr 24, 2020
 
 @author: RayL
 '''
-from board import Board
+from gameChess import GameChess
 import pygame
-test = 2
-test = 2
 pygame.init()
 window_x = 400
 window_y = 400
 window = pygame.display.set_mode((window_x,window_y)) #Create window
-running = True
-clock = pygame.time.Clock()
-fps = 30
-board = Board(window)
-board.show()
-board.drawBoard()
-board.drawPieces()
-possibleSpaces = []
-isSelected = False
-pieceSelected = 0
-clickBuffer = 0
-turn = 1
-turnCol = 'white'
-oppoCol = 'black'
-while running:
-    clock.tick(fps) #Update the clock by the fps every frame
-    for event in pygame.event.get(): #Loop to check for user exit
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            coords = [x // board.spaceSize for x in pygame.mouse.get_pos()]
-            coords.reverse()
-            clickedSpace = board.board[coords[0]][coords[1]]
-            if not isSelected:
-                if clickedSpace.color == turnCol:
-                    pieceSelected = clickedSpace
-                    if pieceSelected != 0:
-                        possibleSpaces = pieceSelected.getSpaces(board,window)
-                    isSelected = True
-            elif isSelected:
-                if clickedSpace == 0 or clickedSpace == pieceSelected or clickedSpace.color == oppoCol:
-                    if coords in possibleSpaces:
-                        board.move(pieceSelected,coords)
-                        turn += 1
-                    pieceSelected = 0
-                    possibleSpaces = []
-                    isSelected = False
-                    board.drawBoard()
-                    board.drawPieces()
-                else:
-                    board.drawBoard()
-                    board.drawPieces()
-                    if clickedSpace.color == turnCol:
-                        pieceSelected = board.board[coords[0]][coords[1]]
-                        possibleSpaces = pieceSelected.getSpaces(board,window)
-                        isSelected = True
-        if turn % 2 == 1:
-            turnCol = 'white'
-            oppoCol = 'black'
-        else:
-            turnCol = 'black'
-            oppoCol = 'white'
-    pygame.display.update()
-    
-            
-
-            
-            
+game = GameChess(window)
+while game.running:
+    game.play()      
 
             
