037cd8ac63face4ab9aa9ce4352a89ea7f826cfb
rayli1639 <ray.z.li@gmail.com>
2020-05-06 13:56:34 -0400

    Added sprites dict

---
+++ pieceSprites.py
@@ -0,0 +1,21 @@
+'''
+Created on May 5, 2020
+
+@author: RayL
+'''
+import pygame
+
+pieceSprites = {
+            'whitePawn': pygame.image.load('sprites/whitePawn.png'),
+            'blackPawn': pygame.image.load('sprites/blackPawn.png'),
+            'whiteKnight': pygame.image.load('sprites/whiteKnight.png'),
+            'blackKnight': pygame.image.load('sprites/blackKnight.png'),
+            'whiteBishop': pygame.image.load('sprites/whiteBishop.png'),
+            'blackBishop': pygame.image.load('sprites/blackBishop.png'),
+            'whiteRook': pygame.image.load('sprites/whiteRook.png'),
+            'blackRook': pygame.image.load('sprites/blackRook.png'),
+            'whiteQueen': pygame.image.load('sprites/whiteQueen.png'),
+            'blackQueen': pygame.image.load('sprites/blackQueen.png'),
+            'whiteKing': pygame.image.load('sprites/whiteKing.png'),
+            'blackKing': pygame.image.load('sprites/blackKing.png')
+            }
