f949f874b6516b0cdfa53cc5ed6849bd022044cd
rayli1639 <ray.z.li@gmail.com>
2020-05-08 15:59:16 -0400

    Fixed implementation of stalemate for multiplayer

--- gameChess.py
+++ gameChess.py
@@ -35,6 +35,7 @@ class GameChess():
         boardInfo = self.n.send('Receiving Data')
         self.board.board = boardInfo[0]
         self.board.positions = boardInfo[2]
+    
         if self.color == 'white':
             self.isTurn = True
             self.turnCol = 'white'
@@ -107,7 +108,8 @@ class GameChess():
     
     def play(self):
         
-        for row in self.board.board:
+        
+        for row in self.board.board: #Update blackKing and whiteKing
             for piece in row:
                 if piece != 0:
                     if piece.color == 'white' and piece.isKing:
@@ -153,7 +155,11 @@ class GameChess():
                 self.resetBoard()
                 self.isTurn = True
                 self.board.positions = dB[2]
-                print(self.board.positions)
+                
+                for pos in self.board.positions:
+                    if pos[1] == 2:
+                        print('The match ended in a stalemate')
+                        self.running = False
         
         else:
             
@@ -184,32 +190,63 @@ class GameChess():
                             
                             for space in self.possibleSpaces:
                                 if coords[0] == space[0] and coords[1] == space[1]:
+                                    
+                                    if self.board.board[space[0]][space[1]] != 0:
+                                        takes = True
+                                    else:
+                                        takes = False
+                                        
                                     self.drawCoords = self.board.move(self.pieceSelected,space,self.window)
                                     
                                     self.n.send([self.board.board,None])
 
-                                    changed = False
-                                    
-                                    #dBoard = [x[:] for x in self.board.board]
-                                    
+                                    dBoard = [x[:] for x in self.board.board]
                                     for b in self.board.positions:
-                                        if self.board.board == b[0]:
-                                            b[1] += 1
-                                            changed = True
-                                            if b[1] == 2:
-                                                self.possibleStalemate = True
+                                        
+                                        addCount = True
+                                        index = None
+                                        row = 0
+                                        col = 0
+                                        
+                                        while row <= 7:
+                                            while col <= 7:
+                                                if (b[0][row][col] != 0 and 
+                                                    dBoard[row][col] != 0):
+                                                    if (b[0][row][col].type != dBoard[row][col].type or
+                                                        b[0][row][col].color != dBoard[row][col].color):
+                                                        addCount = False
+                                                        row = 7
+                                                        col = 7
+                                                elif (b[0][row][col] == 0 and dBoard[row][col] != 0):
+                                                    addCount = False
+                                                    row = 7
+                                                    col = 7
+                                                elif (b[0][row][col] != 0 and dBoard[row][col] == 0):
+                                                    addCount = False
+                                                    row = 7
+                                                    col = 7
+                                                col += 1
+                                            row += 1
+                                            col = 0
+                                            
+                                        if addCount:
+                                            index = self.board.positions.index(b)
                                             break
                                         
-                                        changed = False
+                                    if addCount:
                                         
-                                    if not changed:
+                                        info = self.n.send([dBoard,2,index]) #2 Signifies adding to stalemate counter
+                                        for pos in info[2]:
+                                            if pos[1] == 2:
+                                                self.possibleStalemate = True                           
+                                                
+                                    else:
                                         
-                                        #dBoard = [x[:] for x in self.board.board]
-                                        self.n.send([[self.board.board,0],1])
+                                        self.n.send([[dBoard,0],0]) #0 Signifies adding a board state
                                         
-                                    if self.pieceSelected.isPawn:
-                                        #dBoard = [x[:] for x in self.board.board]
-                                        self.n.send([[self.board.board,0],1])
+                                    if self.pieceSelected.isPawn or takes:
+                                        
+                                        self.n.send([[dBoard,0],1]) #1 Signifies resetting stalemate counter
                     
                                     self.isTurn = False
                                     
@@ -229,53 +266,5 @@ class GameChess():
                     if piece != 0:
                         if piece.isPawn and piece.color != self.turnCol:
                             piece.canEnPassant = [False,[0,0]]
-                                    
-#                 if self.turn % 2 == 1:
-#                     
-#                     if isKingChecked(self.board.whiteKing,self.board,None,None):
-#                         self.whiteChecked = True
-#                     else:
-#                         self.whiteChecked = False
-#                         
-#                     self.turnCol = 'white'
-#                     self.oppoCol = 'black'
-#                     
-#                     for row in self.board.board:
-#                         for piece in row:
-#                             if piece != 0:
-#                                 if piece.isPawn and piece.color != self.turnCol:
-#                                     piece.canEnPassant = [False,[0,0]]
-#                                     
-#                 else:
-#             
-#                     if isKingChecked(self.board.blackKing,self.board,None,None):
-#                         self.blackChecked = True
-#                     else:
-#                         self.blackChecked = False
-#                         
-#                     self.turnCol = 'black'
-#                     self.oppoCol = 'white'
-#                     
-#                     for row in self.board.board:
-#                         for piece in row:
-#                             if piece != 0:
-#                                 if piece.isPawn and piece.color != self.turnCol:
-#                                     piece.canEnPassant = [False,[0,0]]
-                                    
-#                 if self.isMated():
-#         
-#                     if self.blackChecked:  
-#                         print('white has won. black was checkmated.')
-#                     elif self.whiteChecked:
-#                         print('black has won. white was checkmated')
-#                     else:
-#                         print('The match ended in a stalemate')
-#                     self.running = False
-#                     break
-#                 
-#                 if self.possibleStalemate:
-#                     print('The match ended in a stalemate')
-#                     self.running = False
-#                     break
             
         pygame.display.update()
